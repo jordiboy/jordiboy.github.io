931cde4f358952070a6c9bc9f75a227084cfbed3
jordiboy <i_jordanov@yahoo.com>
2022-05-27 11:35:48 +0300

    Added guess the number - console game.

---
+++ ConsoleGames/GuesTheNumber-ConsoleGame/GuesTheNumber-ConsoleGame/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GuessTheNumber_ConsoleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(0, 100);
+
+            int counter = 0;
+            int guess = 0;
+
+            Console.WriteLine("Guess the number between 0 and 100");            
+
+            while (guess != num)
+            {
+                Console.Write("You guess: ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess < num)
+                {
+                    Console.WriteLine("No, The number is bigger.");
+                }
+                else if (guess > num)
+                {
+                    Console.WriteLine("No, The number is smaller.");
+                }
+
+                counter++;
+            }
+            
+            Console.WriteLine("You win!");            
+            Console.WriteLine($"The number is: {num}, and you guess it from {counter} time.");
+
+            Console.ReadLine();
+        }
+    }
+}
